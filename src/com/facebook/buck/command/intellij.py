28587ddf29bb247d86dd6d6e57e48ed3efd55055
David Zeng <david.zeng@addepar.com>
2017-05-30 12:09:50 -0700

    Remove misc.xml generation

--- src/com/facebook/buck/command/intellij.py
+++ src/com/facebook/buck/command/intellij.py
@@ -7,45 +7,6 @@ import sys
 from collections import defaultdict
 
 
-MISC_XML = """<?xml version="1.0" encoding="UTF-8"?>
-<project version="4">
-  <component name="NullableNotNullManager">
-    <option name="myDefaultNullable" value="org.jetbrains.annotations.Nullable" />
-    <option name="myDefaultNotNull" value="org.jetbrains.annotations.NotNull" />
-    <option name="myNullables">
-      <value>
-        <list size="5">
-          <item index="0" class="java.lang.String" itemvalue="org.jetbrains.annotations.Nullable" />
-          <item index="1" class="java.lang.String" itemvalue="javax.annotation.Nullable" />
-          <item index="2" class="java.lang.String" itemvalue="edu.umd.cs.findbugs.annotations.Nullable" />
-          <item index="3" class="java.lang.String" itemvalue="android.support.annotation.Nullable" />
-          <item index="4" class="java.lang.String" itemvalue="javax.annotation.CheckForNull" />
-        </list>
-      </value>
-    </option>
-    <option name="myNotNulls">
-      <value>
-        <list size="4">
-          <item index="0" class="java.lang.String" itemvalue="org.jetbrains.annotations.NotNull" />
-          <item index="1" class="java.lang.String" itemvalue="javax.annotation.Nonnull" />
-          <item index="2" class="java.lang.String" itemvalue="edu.umd.cs.findbugs.annotations.NonNull" />
-          <item index="3" class="java.lang.String" itemvalue="android.support.annotation.NonNull" />
-        </list>
-      </value>
-    </option>
-  </component>
-  <component
-    name="ProjectRootManager"
-    version="2"
-    languageLevel="%(java_language_level)s"
-    assert-keyword="true"
-    jdk-15="true"
-    project-jdk-name="%(project_jdk_name)s"
-    project-jdk-type="%(project_jdk_type)s">
-    %(project_output_url)s
-  </component>
-</project>"""
-
 MODULE_XML_START = """<?xml version="1.0" encoding="UTF-8"?>
 <module type="%(type)s" version="4">"""
 
@@ -494,20 +455,6 @@ def write_all_modules(modules):
     write_file_if_changed('.idea/modules.xml', xml)
 
 
-def write_misc_file(java_settings):
-    """Writes a misc.xml file to define some settings specific to the project."""
-    output_url = '<output url="file://$PROJECT_DIR$/' + \
-        java_settings.get('outputUrl', 'build-ij/classes') + '" />'
-    xml = MISC_XML % {
-        'java_language_level': java_settings.get('languageLevel', 'JDK_1_6'),
-        'project_jdk_name': java_settings.get('jdkName', 'Android API 21 Platform'),
-        'project_jdk_type': java_settings.get('jdkType', 'Android SDK'),
-        'project_output_url': output_url
-    }
-
-    write_file_if_changed('.idea/misc.xml', xml)
-
-
 def write_aars(aars):
     """Writes an XML file to define each prebuilt aar."""
     mkdir_p('.idea/libraries')
@@ -641,9 +588,6 @@ if __name__ == '__main__':
     write_modules(modules, generate_minimum_project, android_auto_generation_disabled)
     write_all_modules(modules)
     write_run_configs()
-
-    java_settings = parsed_json['java']
-    write_misc_file(java_settings)
 
     # Write the list of modified files to stdout
     for path in MODIFIED_FILES:
