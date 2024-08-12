3d73b26197115197d629462a98a94cf58e4dc7bf
Zhenfei Tai <zhenfei.tai@addepar.com>
2024-08-12 12:02:25 -0700

    upd

--- src/com/facebook/buck/testrunner/BaseRunner.java
+++ src/com/facebook/buck/testrunner/BaseRunner.java
@@ -99,15 +99,14 @@ public abstract class BaseRunner {
     doc.appendChild(root);
 
     for (TestResult result : results) {
-      if (result.testMethodName == null || result.testMethodName.isEmpty()) {
-        continue;
-      }
       Element test = doc.createElement("test");
-      test.setAttribute("mytest.method", result.testMethodName);
-      test.setAttribute("mytest.class", result.testClassName);
 
       // suite attribute
-      test.setAttribute("suite", result.testClassName);
+      test.setAttribute(
+        "suite",
+        (result.testMethodName == null && result.testClassName.equals("null"))
+          ? testClassName
+          : result.testClassName);
 
       // name attribute
       test.setAttribute("name", result.testMethodName);
--- src/com/facebook/buck/testrunner/Junit5TestListener.java
+++ src/com/facebook/buck/testrunner/Junit5TestListener.java
@@ -152,8 +152,9 @@ public class Junit5TestListener implements TestExecutionListener {
     }
 
     String className = testClass.getCanonicalName();
-    String methodName =
-      testIdentifier.isTest() ? testIdentifier.getLegacyReportingName().replace("()", "") : "";
+//    String methodName =
+//      testIdentifier.isTest() ? testIdentifier.getLegacyReportingName().replace("()", "") : "";
+    String methodName = testIdentifier.getLegacyReportingName().replace("()", "");
     long runTime = System.currentTimeMillis() - this.testStartTime;
     results.add(
       new TestResult(
